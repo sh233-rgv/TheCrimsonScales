c5fa53947b7a84fe6889fc526c1cc10f82f7c7f1
Zorgovskiy <zorgovskiy@gmail.com>
2025-08-22 11:57:15 +0200

    Implement several non-complex items (#11)
    * Implement several non-complex items

--- Game/Content/Items/Prosperity1/013_MinorStaminaPotion.cs
+++ Game/Content/Items/Prosperity1/013_MinorStaminaPotion.cs
@@ -1,3 +1,5 @@
+using System.Linq;
+
 public class MinorStaminaPotion : Prosperity1Item
 {
 	public override string Name => "Minor Stamina Potion";
@@ -8,4 +10,23 @@ public class MinorStaminaPotion : Prosperity1Item
 	public override ItemUseType ItemUseType => ItemUseType.Consume;
 
 	protected override int AtlasIndex => 26;
+
+	protected override void Subscribe()
+	{
+		base.Subscribe();
+
+		SubscribeDuringTurn(
+			canApply: character => character == Owner && character.Cards.Count(card => card.CardState == CardState.Discarded) > 0,
+			apply: async character =>
+			{
+				await Use(async user =>
+				{
+					AbilityCard selectedAbilityCard =
+						await AbilityCmd.SelectAbilityCard(user, CardState.Discarded, mandatory: true, hintText: $"Select a discarded card to recover");
+
+					await AbilityCmd.ReturnToHand(selectedAbilityCard);
+				});
+			}
+		);
+	}
 }
--- Game/Content/Items/Prosperity2/019_WeightedNet.cs
+++ Game/Content/Items/Prosperity2/019_WeightedNet.cs
@@ -1,3 +1,5 @@
+using Fractural.Tasks;
+
 public class WeightedNet : Prosperity2Item
 {
 	public override string Name => "Weighted Net";
@@ -8,4 +10,22 @@ public class WeightedNet : Prosperity2Item
 	public override ItemUseType ItemUseType => ItemUseType.Spend;
 
 	protected override int AtlasIndex => 8;
+
+	protected override void Subscribe()
+	{
+		base.Subscribe();
+
+		SubscribeDuringAttack(
+			canApply: state => state.Performer == Owner && state.SingleTargetRangeType == RangeType.Range,
+			apply: async state =>
+			{
+				await Use(async user =>
+				{
+					state.SingleTargetAddCondition(Conditions.Immobilize);
+
+					await GDTask.CompletedTask;
+				});
+			}
+		);
+	}
 }
