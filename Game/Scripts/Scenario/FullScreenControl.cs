cec5ba99464e4e09e9919c9325e6acb177c74ace
Bas <bc.hoogeboom@gmail.com>
2025-09-08 16:30:55 +0200

    Added screen distortion shader and script to help control stay full screen.

---
+++ Game/Scripts/Scenario/FullScreenControl.cs
@@ -0,0 +1,17 @@
+﻿using Godot;
+
+public partial class FullScreenControl : Control
+{
+	public override void _Process(double delta)
+	{
+		UpdateToViewport();
+	}
+
+	private void UpdateToViewport()
+	{
+		// Set the global position and size so the control covers the screen
+		Camera2D camera = GameController.Instance.CameraController.Camera;
+		Size = GetViewport().GetVisibleRect().Size / camera.Zoom;
+		GlobalPosition = (camera.GlobalPosition) - Size * 0.5f;
+	}
+}
