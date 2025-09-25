88a76e215d3f55d003b5f351c0ea327a212a1c7c
Bas Hoogeboom <bc.hoogeboom@gmail.com>
2025-09-25 17:59:39 +0200

    Refactored the card selection popup.

--- Game/Scripts/UI/CardSelectionList/CardSelectionList.cs
+++ Game/Scripts/UI/CardSelectionList/CardSelectionList.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using Godot;
+using GTweens.Easings;
+using GTweensGodot.Extensions;
 
 public partial class CardSelectionList : Control
 {
@@ -18,8 +20,9 @@ public partial class CardSelectionList : Control
 
 	public List<CardSelectionCard> Cards { get; } = new List<CardSelectionCard>();
 
-	private float _itemWidth;
-	private bool _restingEnabled;
+	private Comparison<SavedAbilityCard> _sortComparison;
+
+	private Vector2 _cardSize;
 
 	private event Action<CardSelectionCard> CardPressedEvent;
 	private event Action<CardSelectionCard> InitiativePressedEvent;
@@ -32,12 +35,13 @@ public partial class CardSelectionList : Control
 	}
 
 	public void Open(IEnumerable<SavedAbilityCard> cards,
-		Action<CardSelectionCard> cardPressed, Action<CardSelectionCard> initiativePressed)
+		Action<CardSelectionCard> cardPressed, Action<CardSelectionCard> initiativePressed,
+		Comparison<SavedAbilityCard> sortComparison)
 	{
-		bool wasOpen = false;
+		//bool wasOpen = false;
 		if(Cards.Count > 0)
 		{
-			wasOpen = true;
+			//wasOpen = true;
 
 			foreach(CardSelectionCard item in Cards)
 			{
@@ -48,35 +52,24 @@ public partial class CardSelectionList : Control
 			Cards.Clear();
 		}
 
+		_sortComparison = sortComparison;
+
 		CardPressedEvent = cardPressed;
 		InitiativePressedEvent = initiativePressed;
 
 		List<SavedAbilityCard> sortedCards = cards.ToList();
-		// sortedCards.Sort((cardA, cardB) =>
-		// 	cardA.CardState.CompareTo(cardB.CardState) * 10 + cardA.Model.Initiative.CompareTo(cardB.Model.Initiative));
+		sortedCards.Sort(_sortComparison);
 
 		int index = 0;
-		float cardSize = 0f;
 		foreach(SavedAbilityCard savedAbilityCard in sortedCards)
 		{
-			CardSelectionCard card = _cardSelectionCardScene.Instantiate<CardSelectionCard>();
-			_cardParent.AddChild(card);
-			cardSize = card.Size.Y;
-			card.Position = new Vector2(0f, index * card.Size.Y);
-			card.Init(savedAbilityCard, true, InitiativePressedEvent != null);
-			card.CardPressedEvent += OnCardPressed;
-			card.InitiativePressedEvent += OnInitiativePressed;
-			card.MouseEnteredEvent += OnMouseEntered;
-			card.MouseExitedEvent += OnMouseExited;
-
+			CardSelectionCard card = CreateCard(savedAbilityCard, GetPosition(index));
 			Cards.Add(card);
 			index++;
-
-			_itemWidth = card.Size.X;
 		}
 
-		_scrollContainer.Size = new Vector2(_itemWidth, _scrollContainer.Size.Y);
-		_cardParent.CustomMinimumSize = new Vector2(_itemWidth, index * cardSize);
+		_scrollContainer.Size = new Vector2(_cardSize.X, _scrollContainer.Size.Y);
+		_cardParent.CustomMinimumSize = new Vector2(_cardSize.X, index * _cardSize.Y);
 		_cardParent.Size = _cardParent.CustomMinimumSize;
 		_scrollContainer.MouseFilter = _cardParent.Size.Y > _scrollContainer.Size.Y ? MouseFilterEnum.Stop : MouseFilterEnum.Ignore;
 		_cardParent.MouseFilter = _cardParent.Size.Y > _scrollContainer.Size.Y ? MouseFilterEnum.Pass : MouseFilterEnum.Stop;
@@ -93,12 +86,70 @@ public partial class CardSelectionList : Control
 
 		Cards.Clear();
 
-		_cardParent.CustomMinimumSize = new Vector2(_itemWidth, 0f);
+		_cardParent.CustomMinimumSize = new Vector2(_cardSize.X, 0f);
 		_cardParent.Size = _cardParent.CustomMinimumSize;
 		_scrollContainer.MouseFilter = MouseFilterEnum.Ignore;
 
 		CardPressedEvent = null;
 		InitiativePressedEvent = null;
+	}
+
+	public void AddCard(SavedAbilityCard savedAbilityCard)
+	{
+		List<SavedAbilityCard> sortedCards = Cards.Select(cardSelectionCard => cardSelectionCard.SavedAbilityCard).ToList();
+		sortedCards.Add(savedAbilityCard);
+		sortedCards.Sort(_sortComparison);
+
+		int index = sortedCards.IndexOf(savedAbilityCard);
+		CardSelectionCard card = CreateCard(savedAbilityCard, GetPosition(index));
+		Cards.Insert(index, card);
+
+		ReorderCardsVisually();
+	}
+
+	public void RemoveCard(SavedAbilityCard savedAbilityCard)
+	{
+		for(int i = 0; i < Cards.Count; i++)
+		{
+			CardSelectionCard cardSelectionCard = Cards[i];
+			if(cardSelectionCard.SavedAbilityCard == savedAbilityCard)
+			{
+				cardSelectionCard.QueueFree();
+				Cards.RemoveAt(i);
+				break;
+			}
+		}
+
+		ReorderCardsVisually();
+	}
+
+	private CardSelectionCard CreateCard(SavedAbilityCard savedAbilityCard, float positionY)
+	{
+		CardSelectionCard card = _cardSelectionCardScene.Instantiate<CardSelectionCard>();
+		_cardParent.AddChild(card);
+		_cardSize = card.Size;
+		card.Position = new Vector2(0f, positionY);
+		card.Init(savedAbilityCard, true, InitiativePressedEvent != null);
+		card.CardPressedEvent += OnCardPressed;
+		card.InitiativePressedEvent += OnInitiativePressed;
+		card.MouseEnteredEvent += OnMouseEntered;
+		card.MouseExitedEvent += OnMouseExited;
+
+		return card;
+	}
+
+	private float GetPosition(int index)
+	{
+		return index * _cardSize.Y;
+	}
+
+	private void ReorderCardsVisually()
+	{
+		for(int i = 0; i < Cards.Count; i++)
+		{
+			CardSelectionCard card = Cards[i];
+			card.TweenPositionY(GetPosition(i), 0.3f).SetEasing(Easing.OutQuad).Play();
+		}
 	}
 
 	private void OnCardPressed(CardSelectionCard card)
--- Game/Scripts/UI/Popups/CardSelectionPopup/CardSelectionPopup.cs
+++ Game/Scripts/UI/Popups/CardSelectionPopup/CardSelectionPopup.cs
@@ -10,7 +10,7 @@ public partial class CardSelectionPopup : Popup<CardSelectionPopup.Request>
 	}
 
 	[Export]
-	private CardSelectionList _selectedCardList;
+	private CardSelectionList _handCardList;
 	[Export]
 	private CardSelectionList _availableCardList;
 
@@ -23,7 +23,12 @@ public partial class CardSelectionPopup : Popup<CardSelectionPopup.Request>
 
 		List<SavedAbilityCard> handCards = PopupRequest.SavedCharacter.HandAbilityCardIndices
 			.Select(cardIndex => PopupRequest.SavedCharacter.AvailableAbilityCards[cardIndex]).ToList();
-		_selectedCardList.Open(handCards, OnCardPressed, null);
+		_handCardList.Open(handCards, OnHandCardPressed, null,
+			(cardA, cardB) => cardA.Model.Initiative.CompareTo(cardB.Model.Initiative));
+
+		List<SavedAbilityCard> availableCards = PopupRequest.SavedCharacter.AvailableAbilityCards.Where(card => !handCards.Contains(card)).ToList();
+		_availableCardList.Open(availableCards, OnAvailableCardPressed, null,
+			(cardA, cardB) => cardA.Model.Initiative.CompareTo(cardB.Model.Initiative));
 
 		UpdateLabel();
 	}
@@ -31,11 +36,9 @@ public partial class CardSelectionPopup : Popup<CardSelectionPopup.Request>
 	protected override void OnClosed()
 	{
 		PopupRequest.SavedCharacter.HandAbilityCardIndices.Clear();
-		foreach(CardSelectionCard card in _selectedCardList.Cards)
+		foreach(CardSelectionCard card in _handCardList.Cards)
 		{
-			SavedAbilityCard savedAbilityCard =
-				PopupRequest.SavedCharacter.AvailableAbilityCards.Find(savedAbilityCard => savedAbilityCard.Model == card.SavedAbilityCard.Model);
-			PopupRequest.SavedCharacter.HandAbilityCardIndices.Add(PopupRequest.SavedCharacter.AvailableAbilityCards.IndexOf(savedAbilityCard));
+			PopupRequest.SavedCharacter.HandAbilityCardIndices.Add(PopupRequest.SavedCharacter.AvailableAbilityCards.IndexOf(card.SavedAbilityCard));
 		}
 
 		base.OnClosed();
@@ -43,41 +46,9 @@ public partial class CardSelectionPopup : Popup<CardSelectionPopup.Request>
 		AppController.Instance.SaveFile.Save();
 	}
 
-	// protected override void AddCards()
-	// {
-	// 	List<AbilityCardModel> handCards =
-	// 		PopupRequest.SavedCharacter.HandAbilityCardIndices.Select(cardIndex => PopupRequest.SavedCharacter.AvailableAbilityCards[cardIndex].Model)
-	// 			.ToList();
-	// 	foreach(AbilityCardModel abilityCardModel in handCards)
-	// 	{
-	// 		AddCard(abilityCardModel, true, _leftCards);
-	// 	}
-	//
-	// 	List<AbilityCardModel> availableCards =
-	// 		PopupRequest.SavedCharacter.AvailableAbilityCards.Select(card => card.Model).Where(card => !handCards.Contains(card)).ToList();
-	// 	foreach(AbilityCardModel abilityCardModel in availableCards)
-	// 	{
-	// 		AddCard(abilityCardModel, false, _rightCards);
-	// 	}
-	// }
-
-	// protected override void SortList(List<CardSelectionPopupCard> cards, bool left)
-	// {
-	// 	//IList<AbilityCardModel> classAbilityCards = PopupRequest.SavedCharacter.ClassModel.AbilityCards;
-	//
-	// 	//cards.Sort((cardA, cardB) => classAbilityCards.IndexOf(cardA.AbilityCardModel).CompareTo(classAbilityCards.IndexOf(cardB.AbilityCardModel)));
-	// 	cards.Sort((cardA, cardB) => cardA.AbilityCardModel.Initiative.CompareTo(cardB.AbilityCardModel.Initiative));
-	//
-	// 	for(int i = 0; i < cards.Count; i++)
-	// 	{
-	// 		CardSelectionPopupCard card = cards[i];
-	// 		card.GetParent().MoveChild(card, i);
-	// 	}
-	// }
-
 	private void UpdateLabel()
 	{
-		int currentHandSize = _selectedCardList.Cards.Count;
+		int currentHandSize = _handCardList.Cards.Count;
 		int requiredHandSize = PopupRequest.SavedCharacter.ClassModel.HandSize;
 		_requiredCountLabel.SetText($"{currentHandSize}/{requiredHandSize}");
 
@@ -87,8 +58,19 @@ public partial class CardSelectionPopup : Popup<CardSelectionPopup.Request>
 		SetCanClose(correctHandSize);
 	}
 
-	private void OnCardPressed(CardSelectionCard card)
+	private void OnHandCardPressed(CardSelectionCard card)
 	{
+		_handCardList.RemoveCard(card.SavedAbilityCard);
+		_availableCardList.AddCard(card.SavedAbilityCard);
+
+		UpdateLabel();
+	}
+
+	private void OnAvailableCardPressed(CardSelectionCard card)
+	{
+		_availableCardList.RemoveCard(card.SavedAbilityCard);
+		_handCardList.AddCard(card.SavedAbilityCard);
+
 		UpdateLabel();
 	}
 }
