ff685efc70529e5a61060ad32ce661f03da54690
Bas Hoogeboom <bc.hoogeboom@gmail.com>
2025-10-05 14:18:50 +0200

    Fixed item effect button issues.

--- Game/Scripts/Scenario/UI/EffectSelectionView/ItemEffectButton.cs
+++ Game/Scripts/Scenario/UI/EffectSelectionView/ItemEffectButton.cs
@@ -17,10 +17,22 @@ public partial class ItemEffectButton : EffectButton<ItemEffectButton.Parameters
 	[Export]
 	private ItemView _itemView;
 
+	[Export]
+	private Control _iconContainer;
+	[Export]
+	private TextureRect _iconTextureRect;
+
 	protected override void Init(Parameters parameters)
 	{
 		base.Init(parameters);
 
 		_itemView.SetItem(parameters.ItemModel);
+
+		_iconContainer.SetVisible(parameters.ItemModel.Owner != null);
+		if(parameters.ItemModel.Owner != null)
+		{
+			_iconTextureRect.SetTexture(parameters.ItemModel.Owner.ClassModel.IconTexture);
+			_iconTextureRect.SetSelfModulate(parameters.ItemModel.Owner.ClassModel.PrimaryColor);
+		}
 	}
 }
