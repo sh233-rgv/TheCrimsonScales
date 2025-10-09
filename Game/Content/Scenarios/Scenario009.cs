5f7b07d62fd947f730f7a7d5f1557ea9e7aaa806
Bas Hoogeboom <bc.hoogeboom@gmail.com>
2025-10-09 21:03:37 +0200

    Added scenario 13.

--- Game/Content/Scenarios/Scenario009.cs
+++ Game/Content/Scenarios/Scenario009.cs
@@ -8,9 +8,10 @@ public class Scenario009 : ScenarioModel
 	public override string ScenePath => "res://Content/Scenarios/Scenario009.tscn";
 	public override int ScenarioNumber => 9;
 	public override ScenarioChain ScenarioChain => ModelDB.ScenarioChain<MainCampaignScenarioChain>();
-	public override IEnumerable<ScenarioConnection> Connections => [];
+	public override IEnumerable<ScenarioConnection> Connections => [new ScenarioConnection<Scenario013>()];
 
-	protected override ScenarioGoals CreateScenarioGoals() => new CustomScenarioGoals("Kill all revealed enemies and loot the treasure chest to win this scenario.");
+	protected override ScenarioGoals CreateScenarioGoals() =>
+		new CustomScenarioGoals("Kill all revealed enemies and loot the treasure chest to win this scenario.");
 
 	private bool _lootedTreasure;
 	private readonly List<Door> _firstDoors = new List<Door>();
@@ -112,7 +113,8 @@ public class Scenario009 : ScenarioModel
 
 				foreach(Hex otherHex in hexes)
 				{
-					int otherDistance = Mathf.Min(RangeHelper.Distance(_firstDoors[0].Hex, otherHex), RangeHelper.Distance(_firstDoors[1].Hex, otherHex));
+					int otherDistance = Mathf.Min(RangeHelper.Distance(_firstDoors[0].Hex, otherHex),
+						RangeHelper.Distance(_firstDoors[1].Hex, otherHex));
 					if(otherDistance > minDistance)
 					{
 						break;
---
+++ Game/Content/Scenarios/Scenario013.cs
@@ -0,0 +1,39 @@
+using System.Collections.Generic;
+using Fractural.Tasks;
+
+public class Scenario013 : ScenarioModel
+{
+	public override string ScenePath => "res://Content/Scenarios/Scenario013.tscn";
+	public override int ScenarioNumber => 13;
+	public override ScenarioChain ScenarioChain => ModelDB.ScenarioChain<MainCampaignScenarioChain>();
+
+	protected override ScenarioGoals CreateScenarioGoals() => new KillAlLEnemiesScenarioGoals();
+
+	public override string BGSPath => "res://Audio/BGS/Cave.ogg";
+
+	public override async GDTask Start()
+	{
+		await base.Start();
+
+		GameController.Instance.Map.Treasures[0].SetItemLoot(AbilityCmd.GetRandomAvailableStone());
+
+		foreach(Character character in GameController.Instance.CharacterManager.Characters)
+		{
+			await AbilityCmd.AddCondition(null, character, Conditions.Poison1);
+		}
+
+		ScenarioEvents.AbilityStartedEvent.Subscribe(this,
+			parameters =>
+				parameters.AbilityState is AttackAbility.State &&
+				parameters.Performer is Monster monster &&
+				monster.MonsterModel == ModelDB.Monster<LivingCorpse>(),
+			async parameters =>
+			{
+				AttackAbility.State attackAbilityState = (AttackAbility.State)parameters.AbilityState;
+				attackAbilityState.AdjustTargets(1);
+
+				await GDTask.CompletedTask;
+			}
+		);
+	}
+}
