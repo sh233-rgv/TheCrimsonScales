959d207f3d049e6a2160564306697ff7ae3b6ca5
Bas <bc.hoogeboom@gmail.com>
2025-10-13 15:27:44 +0200

    Started on scenario 10.

---
+++ Game/Content/Scenarios/Scenario010.cs
@@ -0,0 +1,29 @@
+using System.Collections.Generic;
+using Fractural.Tasks;
+
+public class Scenario010 : ScenarioModel
+{
+	public override string ScenePath => "res://Content/Scenarios/Scenario010.tscn";
+	public override int ScenarioNumber => 10;
+	public override ScenarioChain ScenarioChain => ModelDB.ScenarioChain<WondrousScenarioChain>();
+	//public override IEnumerable<ScenarioConnection> Connections => [new ScenarioConnection<Scenario002>(true)];
+
+	protected override ScenarioGoals CreateScenarioGoals() => new KillAlLEnemiesScenarioGoals();
+
+	public override string BGSPath => "res://Audio/BGS/Forest Day.ogg";
+
+	public override async GDTask StartBeforeFirstRoomRevealed()
+	{
+		await base.StartBeforeFirstRoomRevealed();
+
+		List<Objective> objectives = GameController.Instance.Map.GetChildrenOfType<Objective>();
+		int objectiveHealth =
+			2 * (GameController.Instance.CharacterManager.Characters.Count + GameController.Instance.SavedScenario.ScenarioLevel + 1);
+		foreach(Objective objective in objectives)
+		{
+			objective.Init(objectiveHealth);
+		}
+
+		GameController.Instance.Map.Treasures[0].SetItemLoot(AbilityCmd.GetRandomAvailableStone());
+	}
+}
---
+++ Game/Content/Scenarios/ScenarioChains/WondrousScenarioChain.cs
@@ -0,0 +1,7 @@
+﻿using Godot;
+
+public class WondrousScenarioChain : ScenarioChain
+{
+	public override ScenarioChain BaseScenarioChain => ModelDB.ScenarioChain<MainCampaignScenarioChain>();
+	public override Color Color => Color.FromHtml("93c07f");
+}
