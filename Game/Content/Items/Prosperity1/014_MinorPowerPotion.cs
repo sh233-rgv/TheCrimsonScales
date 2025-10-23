eb38f3f9c0cc09b353eba0373e8e0a5ad3cc7565
Bas <bc.hoogeboom@gmail.com>
2025-10-24 00:34:57 +0200

    Fixed items that should apply to an entire attack action.

--- Game/Content/Items/Prosperity1/014_MinorPowerPotion.cs
+++ Game/Content/Items/Prosperity1/014_MinorPowerPotion.cs
@@ -23,6 +23,33 @@ public class MinorPowerPotion : Prosperity1Item
 				{
 					state.AbilityAdjustAttackValue(1);
 
+					object subscriber = new object();
+
+					// Also add 1 to all attacks in the same action
+					ScenarioEvents.AbilityStartedEvent.Subscribe(this, subscriber,
+						parameters =>
+							parameters.AbilityState.ActionState == state.ActionState &&
+							parameters.AbilityState is AttackAbility.State,
+						async parameters =>
+						{
+							AttackAbility.State attackAbilityState = ((AttackAbility.State)parameters.AbilityState);
+							attackAbilityState.AbilityAdjustPull(2);
+
+							await GDTask.CompletedTask;
+						}
+					);
+
+					ScenarioEvents.ActionEndedEvent.Subscribe(this, subscriber,
+						parameters => parameters.ActionState == state.ActionState,
+						async parameters =>
+						{
+							ScenarioEvents.AbilityStartedEvent.Unsubscribe(this, subscriber);
+							ScenarioEvents.ActionEndedEvent.Unsubscribe(this, subscriber);
+
+							await GDTask.CompletedTask;
+						}
+					);
+
 					await GDTask.CompletedTask;
 				});
 			}
