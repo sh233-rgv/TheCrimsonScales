2c4de21712e6d47518984c7baff28669092a5d67
Zorgovskiy <zorgovskiy@gmail.com>
2025-11-03 22:16:27 +0100

    Gelatinous giant (#57)
    * Implement boss cards and start on gelatinous giant
    
    * Cards complete
    
    * Draft
    
    * Beautifications
    
    * Except the giant
    
    * Simplify ability
    
    * Add monster stats and traits
    
    * Add second stage boss
    
    * Update deck
    
    * Change inheritance
    
    * Review comments
    
    * Fix develop
    
    ---------
    
    Co-authored-by: Sergey Burgsdorf <sergey.burgsdorf@gmail.com>

---
+++ Game/Content/Monsters/Boss/BossCards.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections.Generic;
+
+public abstract class BossAbilityCard : MonsterAbilityCardModel
+{
+	public override string CardsAtlasPath => "res://Content/Monsters/Boss/Cards.jpg";
+
+	public static IEnumerable<MonsterAbilityCardModel> Deck { get; } =
+	[
+		ModelDB.MonsterAbilityCard<BossAbilityCard0>(),
+		ModelDB.MonsterAbilityCard<BossAbilityCard1>(),
+		ModelDB.MonsterAbilityCard<BossAbilityCard2>(),
+		ModelDB.MonsterAbilityCard<BossAbilityCard3>(),
+		ModelDB.MonsterAbilityCard<BossAbilityCard4>(),
+		ModelDB.MonsterAbilityCard<BossAbilityCard5>(),
+		ModelDB.MonsterAbilityCard<BossAbilityCard6>(),
+		ModelDB.MonsterAbilityCard<BossAbilityCard7>(),
+	];
+}
+
+public class BossAbilityCard0 : BossAbilityCard
+{
+	public override int Initiative => 11;
+	public override int CardIndex => 0;
+
+	public override IEnumerable<MonsterAbilityCardAbility> GetAbilities(Monster monster) => ((IBossMonsterModel)monster.MonsterModel).GetSpecial2Abilities(monster);
+}
+
+public class BossAbilityCard1 : BossAbilityCard
+{
+	public override int Initiative => 14;
+	public override int CardIndex => 1;
+
+	public override IEnumerable<MonsterAbilityCardAbility> GetAbilities(Monster monster) => ((IBossMonsterModel)monster.MonsterModel).GetSpecial2Abilities(monster);
+}
+
+public abstract class BossAbilityCard2 : BossAbilityCard
+{
+	public override int Initiative => 17;
+	public override int CardIndex => 2;
+	public override bool Reshuffles => true;
+
+	public override IEnumerable<MonsterAbilityCardAbility> GetAbilities(Monster monster) => ((IBossMonsterModel)monster.MonsterModel).GetSpecial2Abilities(monster);
+}
+
+public abstract class BossAbilityCard3 : BossAbilityCard
+{
+	public override int Initiative => 85;
+	public override int CardIndex => 3;
+	public override bool Reshuffles => true;
+
+	public override IEnumerable<MonsterAbilityCardAbility> GetAbilities(Monster monster) => ((IBossMonsterModel)monster.MonsterModel).GetSpecial1Abilities(monster);
+}
+
+public abstract class BossAbilityCard4 : BossAbilityCard
+{
+	public override int Initiative => 79;
+	public override int CardIndex => 4;
+
+	public override IEnumerable<MonsterAbilityCardAbility> GetAbilities(Monster monster) => ((IBossMonsterModel)monster.MonsterModel).GetSpecial1Abilities(monster);
+}
+
+public abstract class BossAbilityCard5 : BossAbilityCard
+{
+	public override int Initiative => 73;
+	public override int CardIndex => 5;
+
+	public override IEnumerable<MonsterAbilityCardAbility> GetAbilities(Monster monster) => ((IBossMonsterModel)monster.MonsterModel).GetSpecial1Abilities(monster);
+}
+
+public abstract class BossAbilityCard6 : BossAbilityCard
+{
+	public override int Initiative => 36;
+	public override int CardIndex => 6;
+
+	public override IEnumerable<MonsterAbilityCardAbility> GetAbilities(Monster monster) =>
+	[
+		new MonsterAbilityCardAbility(MoveAbility(monster, +0)),
+		new MonsterAbilityCardAbility(AttackAbility(monster, +0)),
+	];
+}
+
+public abstract class BossAbilityCard7 : BossAbilityCard
+{
+	public override int Initiative => 52;
+	public override int CardIndex => 7;
+
+	public override IEnumerable<MonsterAbilityCardAbility> GetAbilities(Monster monster) =>
+    [
+        new MonsterAbilityCardAbility(MoveAbility(monster, -1)),
+		new MonsterAbilityCardAbility(AttackAbility(monster, extraDamage: -1, range: 3, targets: 2)),
+    ];
+}
