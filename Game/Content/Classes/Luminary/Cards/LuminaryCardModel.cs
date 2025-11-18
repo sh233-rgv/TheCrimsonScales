40d75c22ba39daa8182755679afce8b63acfee01
Saahil Herrero <sh233@rice.edu>
2025-11-17 23:28:27 -0600

    starting stuff

---
+++ Game/Content/Classes/Luminary/Cards/LuminaryCardModel.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+public abstract class LuminaryCardModel<TTop, TBottom> : AtlasAbilityCardModel<TTop, TBottom>
+	where TTop : LuminaryCardSide, new()
+	where TBottom : LuminaryCardSide, new()
+{
+	protected override string TexturePath => "res://Content/Classes/Luminary/Cards.jpg";
+	protected override int ColumnCount => 8;
+	protected override int RowCount => 4;
+}
+
+public abstract class LuminaryCardSide : AbilityCardSide
+{
+	protected AbilityCardAbility Scuttle(int distance, IReadOnlyCollection<Element> possibleElements)
+	{
+		return new AbilityCardAbility(MoveAbility.Builder()
+			.WithDistance(distance)
+			.WithOnAbilityEndedPerformed(async state =>
+			{
+				await AbilityCmd.InfuseElement(state.Performer, possibleElements);
+			})
+			.Build());
+	}
+	
+}
---
+++ Game/Content/Classes/Luminary/LuminaryModel.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using Godot;
+
+public class LuminaryModel : ClassModel
+{
+	public override string Name => "Luminary";
+	public override MaxHealthValues MaxHealthValues => MaxHealthValues.High;
+	public override int HandSize => 10;
+	public override string AssetPath => "res://Content/Classes/Luminary";
+	public override Color PrimaryColor => Color.FromHtml("b289be");
+	public override Color SecondaryColor => Color.FromHtml("383f74");
+
+	public override PackedScene Scene => ResourceLoader.Load<PackedScene>($"{AssetPath}/Luminary.tscn");
+
+	public override IList<AbilityCardModel> AbilityCards { get; } =
+	[
+		
+	];
+}
