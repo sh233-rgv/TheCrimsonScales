b1eeee15c7bcbc61397e24feb665df23ccb9ea06
Saahil Herrero <sh233@rice.edu>
2025-11-28 00:17:47 -0800

    Move canswap to abilitycmd

--- Game/Content/Items/CS1/027_TranslocationDevice.cs
+++ Game/Content/Items/CS1/027_TranslocationDevice.cs
@@ -25,7 +25,7 @@ public class TranslocationDevice : CS1Item
 					Figure swapped = await AbilityCmd.SelectFigure(character, list =>
 						{
 						list.AddRange(RangeHelper.GetFiguresInRange(character.Hex, 5, false).Where(figure => character.EnemiesWith(figure) &&
-							figure.CanSwapWith(character) && character.CanSwapWith(figure)));
+							AbilityCmd.CanSwap(Owner, figure)));
 						}, mandatory: false, hintText: "Choose an enemy to swap hexes with");
 					if (swapped == null)
 					{
--- Game/Scripts/Scenario/HexObjects/Figure.cs
+++ Game/Scripts/Scenario/HexObjects/Figure.cs
@@ -340,36 +340,6 @@ public abstract partial class Figure : HexObject
 		return Enemies.HasFlag(figure.Alignment);
 	}
 
-	public bool CanSwapWith(Figure swapFigure)
-	{
-		if(swapFigure.Hex.TryGetHexObjectOfType(out Obstacle obstacle) && !_flying)
-		{
-			ScenarioCheckEvents.CanEnterObstacleCheck.Parameters canEnterObstacleParameters =
-				ScenarioCheckEvents.CanEnterObstacleCheckEvent.Fire(
-					new ScenarioCheckEvents.CanEnterObstacleCheck.Parameters(this, swapFigure.Hex, obstacle, true));
-
-			if(!canEnterObstacleParameters.CanEnter)
-			{
-				return false;
-			}
-		}
-
-		List<Figure> otherFigures = swapFigure.Hex.GetHexObjectsOfType<Figure>().ToList();
-		otherFigures.Remove(swapFigure);
-		if(otherFigures.Count() > 0)
-		{
-			ScenarioCheckEvents.CanEnterHexWithFigureCheck.Parameters canEnterHexWithFigureCheckParameters =
-				ScenarioCheckEvents.CanEnterHexWithFigureCheckEvent.Fire(
-					new ScenarioCheckEvents.CanEnterHexWithFigureCheck.Parameters(this, swapFigure.Hex, otherFigures[0], true));
-
-			if(!canEnterHexWithFigureCheckParameters.CanEnter)
-			{
-				return false;
-			}
-		}
-		return true;
-	}
-
 	public virtual void AddCoin()
 	{
 	}
@@ -533,4 +503,9 @@ public abstract partial class Figure : HexObject
 			index++;
 		}
 	}
+
+	public bool IsFlying()
+    {
+        return _flying;
+    }
 }
