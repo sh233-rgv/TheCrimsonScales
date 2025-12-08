e39513dc61c272a00dff663af2f92fbf53cd737c
Bas <bc.hoogeboom@gmail.com>
2025-12-08 17:51:46 +0100

    Implemented shaped text for events.

--- Game/Scripts/BetweenScenarios/Events/ShapedEventText.cs
+++ Game/Scripts/BetweenScenarios/Events/ShapedEventText.cs
@@ -1,17 +1,19 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using Godot;
+using Environment = System.Environment;
 
 [Tool]
 public partial class ShapedEventText : Control
 {
 	[Export]
-	public string Text
+	public Curve TextShapeCurve
 	{
-		get => _text;
+		get => _textShapeCurve;
 		private set
 		{
-			_text = value;
+			_textShapeCurve = value;
 			UpdateText();
 		}
 	}
@@ -39,33 +41,121 @@ public partial class ShapedEventText : Control
 	}
 
 	[Export]
-	public float[] Widths
+	public float LineHeight
 	{
-		get => _widths;
+		get => _lineHeight;
 		private set
 		{
-			_widths = value;
+			_lineHeight = value;
 			UpdateText();
 		}
 	}
 
 	[Export]
-	private RichTextLabel _richTextLabel;
+	private Control _richTextLabelParent;
 
-	private string _text;
+	[Export]
+	private RichTextLabel[] _richTextLabels;
+
+	private Curve _textShapeCurve;
 	private Font _font;
 	private int _fontSize;
-	private float _radius;
-	private float[] _widths;
+	private float _lineHeight;
+
+	public override void _Ready()
+	{
+		base._Ready();
+
+		this.DelayedCall(UpdateText);
+	}
 
 	private void UpdateText()
 	{
-		if(_richTextLabel == null || _widths == null || _font == null)
+		if(_textShapeCurve == null || _font == null || _richTextLabelParent == null)
 		{
 			return;
 		}
 
-		_richTextLabel.SetText(WrapTextToWidths(_text, _widths, _font, _fontSize));
+		foreach(RichTextLabel richTextLabel in _richTextLabels)
+		{
+			richTextLabel.QueueFree();
+		}
+
+		List<RichTextLabel> labels = new List<RichTextLabel>();
+		string testText =
+			"""
+			"Come one, come all, and welcome to the county fair!" a Quatryl with red-and-white facepaint and a clownish blue wig smiles as he waves you in through the entrance. You've decided to take the day off and visit the county fair, which you've enjoyed frequenting as a youth.
+
+			"Step right up and try your luck!" an Inox strongman wielding a giant hammer beckons you forward. "Do you have what it takes to hit the bell?"
+
+			On the other side, an Aesther throws a dart and pops a balloon. "Try your aim! Can you hit the balloon? Find out here!"
+			""";
+		var paragraphMarker = Environment.NewLine + Environment.NewLine;
+		var paragraphs = testText.Split([paragraphMarker],
+			StringSplitOptions.RemoveEmptyEntries);
+		//List<string> paragraphs = testText.Split('\n').ToList();
+		foreach(string paragraph in paragraphs)
+		{
+			List<string> words = paragraph.Split(' ').ToList();
+
+			string currentLine = "";
+			float lineWidth = 0f;
+
+			foreach(string word in words)
+			{
+				int lineIndex = labels.Count;
+				float yOffset = lineIndex * _lineHeight;
+				float curveT = yOffset / _richTextLabelParent.Size.Y;
+				lineWidth = _textShapeCurve.Sample(curveT) * _richTextLabelParent.Size.X;
+
+				string testLine = (currentLine + " " + word).Trim();
+				float testWidth = _font.GetStringSize(testLine, fontSize: _fontSize).X;
+
+				if(testWidth <= lineWidth)
+				{
+					currentLine = testLine;
+				}
+				else
+				{
+					// Line finished, append it
+					labels.Add(CreateLabel(currentLine, false, lineWidth, labels.Count));
+
+					currentLine = word; // Start new line with this word
+				}
+			}
+
+			// Add final line
+			labels.Add(CreateLabel(currentLine, true, lineWidth, labels.Count));
+
+			// Empty line at the end of a paragraph
+			labels.Add(CreateLabel(string.Empty, false, lineWidth, labels.Count));
+		}
+
+		_richTextLabels = labels.ToArray();
+	}
+
+	private RichTextLabel CreateLabel(string line, bool finalLine, float lineWidth, int lineIndex)
+	{
+		RichTextLabel label = new RichTextLabel();
+		_richTextLabelParent.AddChild(label);
+		if(Engine.IsEditorHint())
+		{
+			label.SetOwner(GetTree().EditedSceneRoot);
+		}
+		else
+		{
+			label.SetOwner(GetTree().Root);
+		}
+
+		label.SetPosition(new Vector2((_richTextLabelParent.Size.X - lineWidth) / 2f, lineIndex * _lineHeight));
+		label.SetSize(new Vector2(lineWidth, _lineHeight));
+		label.SetHorizontalAlignment(finalLine ? HorizontalAlignment.Left : HorizontalAlignment.Fill);
+		label.PushFont(_font, _fontSize);
+		label.AppendText(line);
+		label.SetFitContent(true);
+		label.PopAll();
+		return label;
+		//labels.Add(label);
 	}
 
 	private static string WrapTextToWidths(string text, float[] widths, Font font, int fontSize)
