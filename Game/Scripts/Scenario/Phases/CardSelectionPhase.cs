9cf902e7a3a5d5677d5b4fd62a5a797018336f97
Bas <bc.hoogeboom@gmail.com>
2025-12-25 02:01:48 +0100

    Added start round undo step.

--- Game/Scripts/Scenario/Phases/CardSelectionPhase.cs
+++ Game/Scripts/Scenario/Phases/CardSelectionPhase.cs
@@ -144,6 +144,8 @@ public class CardSelectionPhase : ScenarioPhase
 				await card.SetCardState(CardState.Playing);
 			}
 		}
+
+		GameController.Instance.UndoManager.AddStep(new StartRoundUndoStep(_cardSelectionState));
 	}
 
 	private void OnPromptStarted(Character character)
