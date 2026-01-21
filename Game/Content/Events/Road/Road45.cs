16ecd26c9a991b73ac4979c1338c7f45555f847c
Bas <bc.hoogeboom@gmail.com>
2026-01-21 13:53:24 +0100

    Added some more road events.

---
+++ Game/Content/Events/Road/Road45.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+
+public class Road45 : RoadEventModel<Road45.ChoiceA, Road45.ChoiceB>
+{
+	public override int Number => 45;
+
+	public override string Text =>
+		"""
+		It's dark at night and you're traveling near the shorelines when your lamp unexpectedly burns out. Realizing you have not brought enough oil to rekindle the light, you cautiously continue on with your journey when you notice a glowing light source up ahead.
+
+		As you near the light source, you find yourself face-to-face with a giant glowing Lurker. You carefully approach the creature and see a sign posted nearby that reads, 'LUMINARY FOR HIRE: 10 GOLD.'
+		""";
+
+	public class ChoiceA : EventChoiceModel
+	{
+		public override string ChoiceText => "Pay the Lurker to escort you on your journey.";
+
+		public override string GetStoryText(SavedEventState state) =>
+			"""
+			The Lurker happily accepts your sum and accompanies you on the journey, lighting the path ahead.
+
+			When you reach your destination, it doesn't want to leave your side. It seems it's here to stay, and you realize you haven't just hired a light source... you hired a mercenary.
+			""";
+
+		public override List<EventReward> GetRewards(SavedEventState state) =>
+		[
+			new LoseCollectiveGoldEventReward(10),
+			new SpawnEventReward(ModelDB.Monster<Lurker>(), 4)
+		];
+	}
+
+	public class ChoiceB : EventChoiceModel
+	{
+		public override string ChoiceText => "Save your hard-earned coin and take your chances in the dark.";
+
+		public override string GetStoryText(SavedEventState state) =>
+			"""
+			You continue on with your journey, making your way off the shoreline and into a forest when you accidentally stumble into a thorn-bush. Bruised and cut, you finally reach your destination and vow never to travel without extra oil again.
+			""";
+
+		public override List<EventReward> GetRewards(SavedEventState state) =>
+		[
+			new AllStartScenarioWithDamageEventReward(2)
+		];
+	}
+}
--- Game/Content/Items/CS2/030_IronSnare.cs
+++ Game/Content/Items/CS2/030_IronSnare.cs
@@ -1,6 +1,4 @@
-using System.Collections.Generic;
 using System.Linq;
-using Fractural.Tasks;
 
 public class IronSnare : CS2Item
 {
@@ -23,27 +21,18 @@ public class IronSnare : CS2Item
 			{
 				await Use(async user =>
 				{
-					List<Hex> possibleHexes = new();
-					possibleHexes.AddRange(
-						RangeHelper.GetHexesInRange(character.Hex, 3)
-							.Where(hex => hex.IsEmpty())
+					Hex targetHex = await AbilityCmd.SelectHex(
+						user,
+						list =>
+							list.AddRange(RangeHelper.GetHexesInRange(user.Hex, 3)
+								.Where(hex => hex.IsEmpty())),
+						hintText: $"Select a hex to place the trap"
 					);
-					int minCount = 0;
 
-					List<Hex> targetHexes = await AbilityCmd.SelectHexes(
-						character,
-						list => list.AddRange(possibleHexes),
-						minSelectionCount: minCount,
-						maxSelectionCount: 1,
-						autoSelectIfMaxCountIsValidCount: false,
-						hintText:  $"Select a hex to place the trap"
-					);
-					if(targetHexes.Count > 0)
+					if(targetHex != null)
 					{
-						foreach(Hex hex in targetHexes)
-						{
-							await AbilityCmd.CreateTrap(hex, "res://Content/OverlayTiles/Traps/BearTrap1H.tscn", damage: 0, conditions: [Conditions.Immobilize]);
-						}
+						await AbilityCmd.CreateTrap(targetHex, "res://Content/OverlayTiles/Traps/BearTrap1H.tscn",
+							conditions: [Conditions.Immobilize]);
 					}
 				});
 			}
