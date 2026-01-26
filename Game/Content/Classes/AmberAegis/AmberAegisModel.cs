53d0c947cdb6c4af21a67e2f20b5fdc8e0464c9f
Saahil Herrero <sh233@rice.edu>
2026-01-26 02:50:49 -0600

    a

---
+++ Game/Content/Classes/AmberAegis/AmberAegisModel.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using Godot;
+
+public class AmberAegisModel : ClassModel
+{
+	public override string Name => "Amber Aegis";
+	public override MaxHealthValues MaxHealthValues => MaxHealthValues.VeryHigh;
+	public override int HandSize => 11;
+	public override Ancestry Ancestry => Ancestry.Harrower;
+
+	public override string AssetPath => "res://Content/Classes/AmberAegis";
+	public override Color PrimaryColor => Color.FromHtml("ef6b26");
+	public override Color SecondaryColor => Color.FromHtml("4b732e");
+
+	public override PackedScene Scene => ResourceLoader.Load<PackedScene>($"{AssetPath}/AmberAegis.tscn");
+
+	public override IList<AbilityCardModel> AbilityCards { get; } =
+	[
+		ModelDB.AbilityCard<BloodThinner>(),
+		ModelDB.AbilityCard<Bogstep>(),
+		ModelDB.AbilityCard<DeathSentence>(),
+		ModelDB.AbilityCard<GroundSolvent>(),
+		ModelDB.AbilityCard<LashingVines>(),
+		ModelDB.AbilityCard<Mudslide>(),
+		ModelDB.AbilityCard<Neurotoxin>(),
+		ModelDB.AbilityCard<ParalyticAgent>(),
+		ModelDB.AbilityCard<SerpentsKiss>(),
+		ModelDB.AbilityCard<StillRiverAlgae>(),
+
+		ModelDB.AbilityCard<Sinkhole>(),
+		ModelDB.AbilityCard<VolatileTonic>(),
+		ModelDB.AbilityCard<CopperneckBerries>(),
+
+		ModelDB.AbilityCard<ThrowingDaggers>(),
+		ModelDB.AbilityCard<AirborneSpores>(),
+		ModelDB.AbilityCard<PotentMixture>(),
+		ModelDB.AbilityCard<HideAndSeek>(),
+		ModelDB.AbilityCard<RadiantForestFungi>(),
+		ModelDB.AbilityCard<FirerootSap>(),
+	];
+}
--- Game/Scripts/Scenario/HexObjects/MaxHealthValues.cs
+++ Game/Scripts/Scenario/HexObjects/MaxHealthValues.cs
@@ -7,6 +7,7 @@ public class MaxHealthValues
 	public static MaxHealthValues Medium { get; } = new MaxHealthValues([8, 9, 11, 12, 14, 15, 17, 18, 20]);
 	public static MaxHealthValues MediumHigh { get; } = new MaxHealthValues([9, 10, 12, 14, 16, 17, 19, 21, 23]);
 	public static MaxHealthValues High { get; } = new MaxHealthValues([10, 12, 14, 16, 18, 20, 22, 24, 26]);
+	public static MaxHealthValues VeryHigh { get; } = new MaxHealthValues([12, 14, 17, 19, 22, 24, 27, 29, 32]);
 
 	public MaxHealthValues(int[] values)
 	{
