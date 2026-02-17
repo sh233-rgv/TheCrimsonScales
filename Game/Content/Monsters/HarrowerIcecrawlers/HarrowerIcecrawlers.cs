0b3fe594b759072dc2f19422da9503605db13a37
Bas <bc.hoogeboom@gmail.com>
2026-02-17 11:29:12 +0100

    Small fix.

--- Game/Content/Monsters/HarrowerIcecrawlers/HarrowerIcecrawlers.cs
+++ Game/Content/Monsters/HarrowerIcecrawlers/HarrowerIcecrawlers.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 public class HarrowerIcecrawlers : HarrowerInfester
 {
 	public override string Name => "Harrower Icecrawlers";
-	public override MonsterModel ParentMonsterModel => ModelDB.Monster<HarrowerIcecrawlers>();
+	public override MonsterModel ParentMonsterModel => ModelDB.Monster<HarrowerInfester>();
 
 	public override IEnumerable<MonsterAbilityCardModel> Deck => HarrowerIcecrawlersAbilityCard.Deck;
 }
