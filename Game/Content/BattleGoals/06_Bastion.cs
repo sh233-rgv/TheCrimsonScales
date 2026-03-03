e888568e9dd4fc1896db46c792b7311bb65bb566
Bas Hoogeboom <bc.hoogeboom@gmail.com>
2026-03-04 00:20:58 +0100

    Implemented Bastion battle goal.

--- Game/Content/BattleGoals/06_Bastion.cs
+++ Game/Content/BattleGoals/06_Bastion.cs
@@ -1,13 +1,27 @@
-﻿using Fractural.Tasks;
+﻿using System.Linq;
+using Fractural.Tasks;
 
 public class Bastion : TheCrimsonScalesBattleGoal
 {
 	public override string Title => "Bastion";
-	public override string Description => "Be adjacent to at least two monsters while standing on a door hex.";
+	public override string Description => "At the end of any round, be adjacent to at least two monsters while standing on a door hex.";
+
+	public override BattleGoalCheckmarkCount CheckmarkCount => BattleGoalCheckmarkCount.Two;
 
 	public override async GDTask OnScenarioSetupPhaseCompleted(Character character, BattleGoal battleGoal)
 	{
-		//TODO: Implement
+		ScenarioEvents.RoundEndedEvent.Subscribe(this,
+			parameters =>
+				GameController.Instance.Map.Doors.Any(door => door.Hex == character.Hex) &&
+				RangeHelper.GetFiguresInRange(character.Hex, 1, false, false).Count(figure => character.EnemiesWith(figure)) >= 2,
+			async parameters =>
+			{
+				battleGoal.AdjustProgress(1);
+
+				await GDTask.CompletedTask;
+			}
+		);
+
 		await GDTask.CompletedTask;
 	}
 }
