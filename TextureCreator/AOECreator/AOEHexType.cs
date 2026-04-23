3da068c90a3d3692e86590bee890b28c5cbc4f87
Bas <bc.hoogeboom@gmail.com>
2026-04-23 20:54:10 +0200

    Implemented AOE pattern creator.

---
+++ TextureCreator/AOECreator/AOEHexType.cs
@@ -0,0 +1,7 @@
+﻿public enum AOEHexType
+{
+	Red = 1,
+	Gray = 2,
+	Empty = 4,
+	Yellow = 8,
+}
---
+++ TextureCreator/AOECreator/AOEPattern.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+public class AOEPattern
+{
+	public string FileName { get; }
+	public List<AOEHex> LocalHexes { get; }
+
+	public AOEPattern(string fileName, List<AOEHex> localHexes)
+	{
+		FileName = fileName;
+		LocalHexes = localHexes;
+	}
+}
