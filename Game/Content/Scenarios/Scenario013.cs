62c9eeb033f8e5c3f2ec66402897da11b7c18b77
Bas Hoogeboom <bc.hoogeboom@gmail.com>
2026-05-18 14:30:10 +0200

    Reimplemented scenario 13 and 14.

--- Game/Content/Scenarios/Scenario013.cs
+++ Game/Content/Scenarios/Scenario013.cs
@@ -1,61 +1,102 @@
-// using Fractural.Tasks;
-//
-// public class Scenario013 : ScenarioModel
-// {
-// 	public override string ScenePath => "res://Content/Scenarios/Scenario013.tscn";
-// 	public override int ScenarioNumber => 13;
-// 	public override ScenarioChain ScenarioChain => ModelDB.ScenarioChain<MainCampaignScenarioChain>();
-//
-// 	protected override ScenarioGoals CreateScenarioGoals() => new KillAllEnemiesScenarioGoals();
-//
-// 	public override string BGSPath => "res://Audio/BGS/Cave.ogg";
-//
-// 	public override async GDTask StartOfScenarioEffects(Character character)
-// 	{
-// 		await AbilityCmd.AddCondition(null, character, Conditions.Poison1);
-// 	}
-//
-// 	public override async GDTask InitializeAfterFirstRoomRevealed()
-// 	{
-// 		await base.InitializeAfterFirstRoomRevealed();
-//
-// 		UpdateScenarioText($"All Living Corpses add {Icons.Inline(Icons.Targets)} 1 on all their attacks.");
-//
-// 		GameController.Instance.Map.Treasures[0].SetItemLoot(AbilityCmd.GetRandomAvailableStone());
-//
-// 		ScenarioEvents.AbilityStartedEvent.Subscribe(this,
-// 			parameters =>
-// 				parameters.AbilityState is AttackAbility.State &&
-// 				parameters.Performer is Monster monster &&
-// 				monster.MonsterModel == ModelDB.Monster<LivingCorpse>(),
-// 			async parameters =>
-// 			{
-// 				AttackAbility.State attackAbilityState = (AttackAbility.State)parameters.AbilityState;
-// 				attackAbilityState.AdjustTargets(1);
-//
-// 				await GDTask.CompletedTask;
-// 			}
-// 		);
-//
-// 		ScenarioCheckEvents.AIMoveParametersCheckEvent.Subscribe(this,
-// 			parameters =>
-// 				parameters.Performer is Monster monster &&
-// 				monster.MonsterModel == ModelDB.Monster<LivingCorpse>(),
-// 			parameters =>
-// 			{
-// 				parameters.AdjustTargets(1);
-// 			}
-// 		);
-//
-// 		ScenarioCheckEvents.TargetsCheckEvent.Subscribe(this,
-// 			parameters =>
-// 				parameters.Figure is Monster monster &&
-// 				monster.MonsterModel == ModelDB.Monster<LivingCorpse>(),
-// 			parameters =>
-// 			{
-// 				parameters.AdjustTargets(1);
-// 			}
-// 		);
-// 	}
-// }
+using System.Collections.Generic;
+using Fractural.Tasks;
 
+public class Scenario013 : ScenarioModel
+{
+	public override string ScenePath => "res://Content/Scenarios/Scenario013.tscn";
+
+	public override int ScenarioNumber => 13;
+	public override string Name => "Corpse Cavern";
+
+	public override ScenarioChain ScenarioChain => ModelDB.ScenarioChain<MainCampaignScenarioChain>();
+
+	public override string IntroductionText =>
+		"""
+		You are walking through the Sinking Market, trying to avoid the hustlers hawking strange pieces of metal and the like, when you are approached by a slightly flustered Shiela, the potion maker from The Sleeping Lion.
+
+		“Just the people!” she cries out, “I could really do with a favor, if you aren’t too busy.”
+
+		Always keen to help someone so generous with her useful concoctions, you ask her to explain what you can do to help.
+
+		“There is a cavern, not too far from here, which is the only place for miles around that Hanging Moss can be found. This is a most useful ingredient in many of my more advanced potions, but it requires very delicate handling. Unfortunately, my stocks are running low and the cave is populated by some horrible creatures. Could you clear the cavern for me, so that I can harvest the moss?
+
+		“I will give you the choice of my next batch of elixirs,” she adds with a wink. Stepping into a cave full of ‘horrible creatures’ doesn’t sound like enormous fun, but Shiela’s concoctions are the best in the business.
+
+		As you enter the cavern, you start to doubt the wisdom of this trip almost immediately, as you are greeted by shambling re-animated corpses who head for you as soon as you enter the chamber. This had better be the most amazing moss in Gloomhaven.
+		""";
+
+	public override string ConclusionText =>
+		"""
+		Entering the final chamber, the entrance way littered with the bodies of a variety of creatures, you see long, waving tendrils of moss hanging from the ceiling, as well as a few more creatures attempting to kill you. Having dealt with them, you return to look at the moss and gingerly reach out to touch it. It recoils for a second, before detaching itself from the ceiling and wrapping itself around you with a strangling intensity. With some difficulty, you fend it off and retreat to Gloomhaven, letting Shiela know that it is safe to enter the cavern.
+
+		A few days later she sees you again. Looking at the weals the hanging moss has left, she smiles. “I hope I didn’t put you in any trouble,” she says, slightly mischievously. “That moss has incredible powers—help yourself to one of these, and thank you!”
+		""";
+
+	public override List<MonsterModel> MonsterModels { get; } =
+	[
+		ModelDB.Monster<LivingCorpse>(),
+		ModelDB.Monster<Lurker>(),
+		ModelDB.Monster<Ooze>(),
+	];
+
+	public override List<SavedReward> Rewards =>
+	[
+		new GainProsperityReward(1),
+
+		//TODO: Not the true reward for this scenario
+		new GainCollectiveItemReward(ModelDB.Item<MajorStaminaPotion>()),
+		new GainCollectiveItemReward(ModelDB.Item<MajorPowerPotion>()),
+		new GainCollectiveItemReward(ModelDB.Item<MajorManaPotion>()),
+	];
+
+	public override string BGSPath => "res://Audio/BGS/Cave.ogg";
+
+	public override async GDTask StartOfScenarioEffects(Character character)
+	{
+		await AbilityCmd.AddCondition(null, character, Conditions.Poison1);
+	}
+
+	public override async GDTask InitializeAfterFirstRoomRevealed()
+	{
+		await base.InitializeAfterFirstRoomRevealed();
+
+		await AddGoal(new KillAllEnemiesScenarioGoal());
+		AddScenarioRule(textParameters => $"All Living Corpses add {Icons.Inline(Icons.Targets, textParameters)} 1 on all their attacks.");
+
+		GameController.Instance.Map.Treasures[0].SetItemLoot(AbilityCmd.GetRandomAvailableStone());
+
+		ScenarioEvents.AbilityStartedEvent.Subscribe(this,
+			parameters =>
+				parameters.AbilityState is AttackAbility.State &&
+				parameters.Performer is Monster monster &&
+				monster.MonsterModel == ModelDB.Monster<LivingCorpse>(),
+			async parameters =>
+			{
+				AttackAbility.State attackAbilityState = (AttackAbility.State)parameters.AbilityState;
+				attackAbilityState.AdjustTargets(1);
+
+				await GDTask.CompletedTask;
+			}
+		);
+
+		ScenarioCheckEvents.AIMoveParametersCheckEvent.Subscribe(this,
+			parameters =>
+				parameters.Performer is Monster monster &&
+				monster.MonsterModel == ModelDB.Monster<LivingCorpse>(),
+			parameters =>
+			{
+				parameters.AdjustTargets(1);
+			}
+		);
+
+		ScenarioCheckEvents.TargetsCheckEvent.Subscribe(this,
+			parameters =>
+				parameters.Figure is Monster monster &&
+				monster.MonsterModel == ModelDB.Monster<LivingCorpse>(),
+			parameters =>
+			{
+				parameters.AdjustTargets(1);
+			}
+		);
+	}
+}
--- Game/Content/Scenarios/Scenario014.cs
+++ Game/Content/Scenarios/Scenario014.cs
@@ -1,78 +1,117 @@
-// using Fractural.Tasks;
-//
-// public class Scenario014 : ScenarioModel
-// {
-// 	public override string ScenePath => "res://Content/Scenarios/Scenario014.tscn";
-// 	public override int ScenarioNumber => 14;
-// 	public override ScenarioChain ScenarioChain => ModelDB.ScenarioChain<MainCampaignScenarioChain>();
-//
-// 	protected override ScenarioGoals CreateScenarioGoals() =>
-// 		new CustomScenarioGoals("Kill three Cultists to win this scenario.");
-//
-// 	public override string BGSPath => "res://Audio/BGS/Forest Day.ogg";
-//
-// 	private bool _firstDoorOpened;
-// 	private int _cultistMurderCount = 0;
-//
-// 	public override async GDTask InitializeAfterFirstRoomRevealed()
-// 	{
-// 		await base.InitializeAfterFirstRoomRevealed();
-//
-// 		GameController.Instance.Map.Treasures[0].SetItemLoot(ModelDB.Item<DizzyingTincture>());
-// 		GameController.Instance.Map.Treasures[1].SetItemLoot(ModelDB.Item<OrbOfFortune>());
-//
-// 		UpdateScenarioText("The door is locked.\nSomething will happen once all enemies in this room are killed.");
-//
-// 		ScenarioEvents.RoundEndedEvent.Subscribe(this,
-// 			parameters => _cultistMurderCount >= 3,
-// 			async parameters =>
-// 			{
-// 				await ((CustomScenarioGoals)ScenarioGoals).Win();
-// 			}
-// 		);
-//
-// 		ScenarioEvents.FigureKilledEvent.Subscribe(this,
-// 			parameters => true,
-// 			async parameters =>
-// 			{
-// 				if(!_firstDoorOpened)
-// 				{
-// 					foreach(Figure figure in GameController.Instance.Map.Figures)
-// 					{
-// 						if(figure.Alignment == Alignment.Enemies)
-// 						{
-// 							return;
-// 						}
-// 					}
-//
-// 					Door firstDoor = GameController.Instance.Map.GetMarker(Marker.Type._1).Hex.GetHexObjectOfType<Door>();
-// 					await firstDoor.Open(null);
-//
-// 					UpdateScenarioText("Whenever a cultist performs a summon ability, it summons a Living Corpse instead of a Living Bones.");
-//
-// 					_firstDoorOpened = true;
-// 				}
-//
-// 				if(parameters.Figure is Monster monster && monster.MonsterModel == ModelDB.Monster<Cultist>())
-// 				{
-// 					_cultistMurderCount++;
-// 				}
-// 			}
-// 		);
-//
-// 		ScenarioEvents.AbilityStartedEvent.Subscribe(this,
-// 			parameters =>
-// 				parameters.AbilityState is MonsterSummonAbility.State &&
-// 				parameters.Performer is Monster monster &&
-// 				monster.MonsterModel == ModelDB.Monster<Cultist>(),
-// 			async parameters =>
-// 			{
-// 				MonsterSummonAbility.State monsterSummonAbilityState = (MonsterSummonAbility.State)parameters.AbilityState;
-// 				monsterSummonAbilityState.SetMonsterModel(ModelDB.Monster<LivingCorpse>());
-//
-// 				await GDTask.CompletedTask;
-// 			}
-// 		);
-// 	}
-// }
+using System.Collections.Generic;
+using Fractural.Tasks;
 
+public class Scenario014 : ScenarioModel
+{
+	public override string ScenePath => "res://Content/Scenarios/Scenario014.tscn";
+
+	public override int ScenarioNumber => 14;
+	public override string Name => "Cultist Cave";
+
+	public override ScenarioChain ScenarioChain => ModelDB.ScenarioChain<MainCampaignScenarioChain>();
+
+	public override string IntroductionText =>
+		"""
+		You are in the Sleeping Lion, catching up with Shiela and her latest potion developments when the singing Quatryl appears. The other patrons are quite happy at this—they’ll either get a good song, or an opportunity to heckle—but you fail to hide your disgust.
+
+		Enjoying the attention, she skips over to your table, before making an enormous show of presenting a rolled-up note to you:
+
+		“I have received word of a group of zealots meeting in secret just outside the city. They are currently small in number, but are actively recruiting—including one of our own. It is likely that they have some mastery of necromancy, so expect some undead creatures, but there are also likely to be rewards. Keep what you find, but just eliminate the 3 ringleaders. They are meeting tonight. Go straight to the caves marked on the map below and you will find them there—with no other way out.”
+
+		You pass the note around, nod in agreement, drink up and go—still ignoring the singing Quatryl.
+
+		There’s a drunken heckle from the far corner at your sudden leaving, and a cry of “Give us a song, bard!” You make for the door, but as you turn to open it, you see the singing Quatryl standing on your table, guitar in hand.
+
+		As the door closes behind you, you hear her start to sing: “Killing gruesome undead hordes Our heroes dare to go The only trouble they will have Is telling friend from foe”
+
+		You spend your trip to the caves debating how angry Selandre will be when the Quatryl disappears, and imagining increasingly gruesome ways to end her singing career.
+
+		As you reach the place marked on the map, you realize it’s less of an entrance, more of a hole. There’s a low glow coming from the hole, as if lit by torchlight. This is the place.
+
+		Not believing in the ‘look before you leap’ mantra, you jump down—only to find yourself surrounded by monsters. Maybe there is wisdom in the saying after all.
+		""";
+
+	public override string ConclusionText =>
+		"""
+		You step over the last of the bodies, counting your loot, and then look at the hole you dropped through, some ten feet above your heads. Maybe a little more planning wouldn’t go amiss next time…
+		""";
+
+	public override List<MonsterModel> MonsterModels { get; } =
+	[
+		ModelDB.Monster<Cultist>(),
+		ModelDB.Monster<DeepTerror>(),
+		ModelDB.Monster<LivingCorpse>(),
+	];
+
+	public override List<SavedReward> Rewards =>
+	[
+		new GainXPReward(15)
+	];
+
+	public override string BGSPath => "res://Audio/BGS/Cave.ogg";
+
+	private CustomScenarioGoal _goal;
+	private ScenarioRule _somethingWillHappenRule;
+
+	private bool _firstDoorOpened;
+
+	public override async GDTask InitializeAfterFirstRoomRevealed()
+	{
+		await base.InitializeAfterFirstRoomRevealed();
+
+		GameController.Instance.Map.Treasures[0].SetItemLoot(ModelDB.Item<DizzyingTincture>());
+		GameController.Instance.Map.Treasures[1].SetItemLoot(ModelDB.Item<OrbOfFortune>());
+
+		_goal = await AddGoal(new CustomScenarioGoal(textParameters => "Kill 3 Cultists.", hasProgress: true, maxProgress: 3));
+
+		_somethingWillHappenRule = AddScenarioRule("Something will happen once all enemies in this room are killed.");
+
+		ScenarioEvents.FigureKilledEvent.Subscribe(this,
+			parameters => true,
+			async parameters =>
+			{
+				if(!_firstDoorOpened)
+				{
+					foreach(Figure figure in GameController.Instance.Map.Figures)
+					{
+						if(figure.Alignment == Alignment.Monsters)
+						{
+							return;
+						}
+					}
+
+					_somethingWillHappenRule.Remove();
+
+					Door firstDoor = GameController.Instance.Map.GetMarker(Marker.Type._1).Hex.GetHexObjectOfType<Door>();
+					await firstDoor.Open(null);
+
+					await ShowText(
+						"With corpses scattered across the ground, you hear rumbling and take notice of a large stone being removed from what must be a doorway. A hooded figure enters and lets out a small shriek at the sight of your presence. He quickly turns and runs back through the newly revealed doorway. That must be one of the cultists, and you’ll see to it that was the last time he’ll ever shriek.");
+
+					AddScenarioRule("Whenever a cultist performs a summon ability, it summons a Living Corpse instead of a Living Bones.");
+
+					_firstDoorOpened = true;
+				}
+
+				if(parameters.Figure is Monster monster && monster.MonsterModel == ModelDB.Monster<Cultist>())
+				{
+					await _goal.AdjustProgress(1);
+				}
+			}
+		);
+
+		ScenarioEvents.AbilityStartedEvent.Subscribe(this,
+			parameters =>
+				parameters.AbilityState is MonsterSummonAbility.State &&
+				parameters.Performer is Monster monster &&
+				monster.MonsterModel == ModelDB.Monster<Cultist>(),
+			async parameters =>
+			{
+				MonsterSummonAbility.State monsterSummonAbilityState = (MonsterSummonAbility.State)parameters.AbilityState;
+				monsterSummonAbilityState.SetMonsterModel(ModelDB.Monster<LivingCorpse>());
+
+				await GDTask.CompletedTask;
+			}
+		);
+	}
+}
