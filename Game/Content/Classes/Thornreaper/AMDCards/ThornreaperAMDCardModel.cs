6d2cc172ac448af2b45acba0e11a3e4b0950de5d
Saahil Herrero <sh233@rice.edu>
2026-08-04 23:26:23 -0700

    row count

--- Game/Content/Classes/Thornreaper/AMDCards/ThornreaperAMDCardModel.cs
+++ Game/Content/Classes/Thornreaper/AMDCards/ThornreaperAMDCardModel.cs
@@ -2,7 +2,7 @@
 {
 	protected override string GetTexturePath(AMDCardOwner owner) => "res://Content/Classes/Thornreaper/AMDCards.jpg";
 	protected override int ColumnCount => 4;
-	protected override int RowCount => 4;
+	protected override int RowCount => 5;
 
 	protected static bool LightStrongOrWaning =>
 		GameController.Instance.ElementManager.GetState(Element.Light) is ElementState.Strong or ElementState.Waning;
